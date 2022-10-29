b448644e710e769a24979a157cfce985aa0f3e5e
Alex Junior <alexjr2oficial@gmail.com>
2022-10-29 13:10:20 -0300

    updating getLamps.js

---
+++ Christmas-Lights/scripts/getLamps.js
@@ -0,0 +1,31 @@
+// picking up the lamps
+
+const lamp11 = document.getElementById('lamp1-1');
+const lamp12 = document.getElementById('lamp1-2');
+const lamp13 = document.getElementById('lamp1-3');
+const lamp14 = document.getElementById('lamp1-4');
+const lamp15 = document.getElementById('lamp1-5');
+
+const lamp21 = document.getElementById('lamp2-1');
+const lamp22 = document.getElementById('lamp2-2');
+const lamp23 = document.getElementById('lamp2-3');
+const lamp24 = document.getElementById('lamp2-4');
+const lamp25 = document.getElementById('lamp2-5');
+
+const lamp31 = document.getElementById('lamp3-1');
+const lamp32 = document.getElementById('lamp3-2');
+const lamp33 = document.getElementById('lamp3-3');
+const lamp34 = document.getElementById('lamp3-4');
+const lamp35 = document.getElementById('lamp3-5');
+
+const lamp41 = document.getElementById('lamp4-1');
+const lamp42 = document.getElementById('lamp4-2');
+const lamp43 = document.getElementById('lamp4-3');
+const lamp44 = document.getElementById('lamp4-4');
+const lamp45 = document.getElementById('lamp4-5');
+
+const lamp51 = document.getElementById('lamp5-1');
+const lamp52 = document.getElementById('lamp5-2');
+const lamp53 = document.getElementById('lamp5-3');
+const lamp54 = document.getElementById('lamp5-4');
+const lamp55 = document.getElementById('lamp5-5');
