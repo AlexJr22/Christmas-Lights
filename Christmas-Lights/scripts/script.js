e035354c7532be6756bb134df07fb68f1da32a56
Alex Junior <alexjr2oficial@gmail.com>
2022-11-02 14:23:43 -0300

    turn on and off the lamps script

---
+++ Christmas-Lights/scripts/script.js
@@ -0,0 +1,25 @@
+const input = document.getElementById('On_Off');
+
+input.onclick = () => {
+  
+  document.querySelector('.red').classList.toggle('redAnimation');
+  document.querySelector('.blue').classList.toggle('blueAnimation');
+  document.querySelector('.yellow').classList.toggle('yellowAnimation');
+  document.querySelector('.green').classList.toggle('greenAnimation');
+  document.querySelector('.white').classList.toggle('whiteAnimation');
+
+  document.querySelector('.red2').classList.toggle('redAnimation');
+  document.querySelector('.blue2').classList.toggle('blueAnimation');
+  document.querySelector('.yellow2').classList.toggle('yellowAnimation');
+  document.querySelector('.green2').classList.toggle('greenAnimation');
+  document.querySelector('.white2').classList.toggle('whiteAnimation');
+
+  document.querySelector('.red3').classList.toggle('redAnimation');
+  document.querySelector('.blue3').classList.toggle('blueAnimation');
+  document.querySelector('.yellow3').classList.toggle('yellowAnimation');
+  document.querySelector('.green3').classList.toggle('greenAnimation');
+  document.querySelector('.white3').classList.toggle('whiteAnimation');
+
+};
+
+
