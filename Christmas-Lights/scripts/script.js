a46f1907954847f2fa6b89f9f4ff7ae6592f95d5
Alex Junior <alexjr2oficial@gmail.com>
2022-11-03 10:44:54 -0300

    improving the script

--- Christmas-Lights/scripts/script.js
+++ Christmas-Lights/scripts/script.js
@@ -1,26 +1,39 @@
 const input = document.getElementById('On_Off');
 
+const red = document.querySelectorAll('.red');
+const blue = document.querySelectorAll('.blue');
+const yellow = document.querySelectorAll('.yellow');
+const green = document.querySelectorAll('.green');
+const white = document.querySelectorAll('.white');
+
+
 input.onclick = () => {
   
-  document.querySelector('.red').classList.toggle('redAnimation');
-  document.querySelector('.blue').classList.toggle('blueAnimation');
-  document.querySelector('.yellow').classList.toggle('yellowAnimation');
-  document.querySelector('.green').classList.toggle('greenAnimation');
-  document.querySelector('.white').classList.toggle('whiteAnimation');
+  
+  red.forEach(lamps => {
+    addAnimation(lamps, 'redAnimation');
+  });
 
-  document.querySelector('.red2').classList.toggle('redAnimation');
-  document.querySelector('.blue2').classList.toggle('blueAnimation');
-  document.querySelector('.yellow2').classList.toggle('yellowAnimation');
-  document.querySelector('.green2').classList.toggle('greenAnimation');
-  document.querySelector('.white2').classList.toggle('whiteAnimation');
+  blue.forEach(lamps => {
+    addAnimation(lamps, 'blueAnimation');
+  });
 
-  document.querySelector('.red3').classList.toggle('redAnimation');
-  document.querySelector('.blue3').classList.toggle('blueAnimation');
-  document.querySelector('.yellow3').classList.toggle('yellowAnimation');
-  document.querySelector('.green3').classList.toggle('greenAnimation');
-  document.querySelector('.white3').classList.toggle('whiteAnimation');
+  yellow.forEach(lamps => {
+    addAnimation(lamps, 'yellowAnimation');
+  });
+
+  green.forEach(lamps => {
+    addAnimation(lamps, 'greenAnimation');
+  });
+
+  white.forEach(lamps => {
+    addAnimation(lamps, 'whiteAnimation');
+  });
+  
   document.body.classList.toggle('black');
 
 };
 
-
+  function addAnimation(lamps, color = '') {
+    lamps.classList.toggle(color)
+  };
